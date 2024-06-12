58e8a0cc41affc11ae5224170c7fbf3da0999f38
vishalmeti <vishalmeti9@gmail.com>
2024-06-12 22:42:19 +0530

    Nested document creation done.

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -1,9 +1,13 @@
 "use client";
 
 import { Skeleton } from "@/components/ui/skeleton";
+import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { cn } from "@/lib/utils";
-import { ChevronDown, ChevronRight, LucideIcon } from "lucide-react";
+import { useMutation } from "convex/react";
+import { ChevronDown, ChevronRight, LucideIcon, Plus } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 interface ItemProps {
   id?: Id<"documents">;
@@ -30,11 +34,33 @@ const Item = ({
   onExpand,
   expanded,
 }: ItemProps) => {
+  const router = useRouter();
+  const create = useMutation(api.documents.create);
+  const handleExpand = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    e.stopPropagation();
+    onExpand?.();
+  };
 
-  const handleExpand = (e:React.MouseEvent<HTMLDivElement, MouseEvent>) =>{
+  const onCreate = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     e.stopPropagation()
-    onExpand?.()
-  }
+    if (!id) return;
+
+    const promise = create({
+      title: "Untitled",
+      parentDocument: id,
+    }).then((documentId) => {
+      if (!expanded) {
+        onExpand?.();
+      }
+      // router.push(`/document/${documentId}`);
+    });
+
+    toast.promise(promise, {
+      loading: "Creating a new note...",
+      success: "New note created!",
+      error: "Failed to create new note",
+    });
+  };
 
   const ChevronIcon = expanded ? ChevronDown : ChevronRight;
 
@@ -59,28 +85,40 @@ const Item = ({
         </div>
       )}
 
-      {documentIcon ? (<div className="shrink-0 mr-2 text-[18px]" > {documentIcon} </div>) :
-      <Icon className="shrink-0 h-[18px] mr-2 text-muted-foreground" />
-      }
+      {documentIcon ? (
+        <div className="shrink-0 mr-2 text-[18px]"> {documentIcon} </div>
+      ) : (
+        <Icon className="shrink-0 h-[18px] mr-2 text-muted-foreground" />
+      )}
       <span className="truncate">{label}</span>
       {isSearch && (
-        <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100" >
-          <span className="text-xs" >CTRL+</span>K
+        <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
+          <span className="text-xs">CTRL+</span>K
         </kbd>
+      )}
+      {!!id && (
+        <div className="ml-auto flex items-center gap-x-2">
+          <div role="button" onClick={onCreate}  className="opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 ">
+            <Plus className="h-4 w-4 text-muted-foreground" />
+          </div>
+        </div>
       )}
     </div>
   );
 };
 
-Item.Skeleton = function ItemSkeleton({level} : {level?:number}){
-  return(
-    <div className="flex gap-x-2 py-[3px]" style={{
-      paddingLeft:level? `${(level+12)+25}px`: "12px" 
-    }} >
+Item.Skeleton = function ItemSkeleton({ level }: { level?: number }) {
+  return (
+    <div
+      className="flex gap-x-2 py-[3px]"
+      style={{
+        paddingLeft: level ? `${level + 12 + 25}px` : "12px",
+      }}
+    >
       <Skeleton className="h-4 w-4" />
       <Skeleton className="h-4 w-[30%]" />
     </div>
-  )
-}
+  );
+};
 
 export default Item;
