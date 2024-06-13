0156325e1a2211c51785c989c47925541596619a
vishalmeti <vishalmeti9@gmail.com>
2024-06-13 07:39:14 +0530

    Added archiving api

--- convex/documents.ts
+++ convex/documents.ts
@@ -2,18 +2,71 @@ import { v } from "convex/values"
 import { mutation, query } from "./_generated/server"
 import { Doc, Id } from "./_generated/dataModel"
 
-export const get = query({
-    handler: async (ctx) => {
-        const identity = await ctx.auth.getUserIdentity();
+// export const get = query({
+//     handler: async (ctx) => {
+//         const identity = await ctx.auth.getUserIdentity();
+
+//         if (!identity) {
+//             throw new Error("Not authenticated")
+//         }
+
+//         const documents = await ctx.db.query("documents").collect();
+
+//         return documents
+
+//     }
+// })
+
+export const archive = mutation({
+    args:{id:v.id("documents")},
+    handler:async(ctx,args)=>{
+        const identity = await ctx.auth.getUserIdentity()
 
         if (!identity) {
-            throw new Error("Not authenticated")
+            throw new Error("Not authenitcated")
         }
 
-        const documents = await ctx.db.query("documents").collect();
+        const userId = identity.subject;
 
-        return documents
+        const existingDoc = await ctx.db.get(args.id)
 
+        if(!existingDoc){
+            throw new Error("Not found")
+        }
+
+        if(existingDoc.userId !==userId){
+            throw new Error("Unauthorized")
+        }
+
+        const recursiveArchive = async(documentId : Id<"documents">)=>{
+            
+            //getting all the child id
+            const children = await ctx.db
+            .query("documents")
+            .withIndex("by_user_parent",(q)=>(
+                q.eq("userId",userId).eq("parentDocument",documentId)
+            )
+            )
+            .collect()
+
+            //looping over all the child and archiving them.
+            for(const child of children){
+                await ctx.db.patch(child._id,{
+                    isArchived:true,
+                })
+
+                //recursivly calling this function on the child for archiving its children
+                await recursiveArchive(child._id)
+            }
+        }
+        
+        
+        const document = await ctx.db.patch(args.id,{
+            isArchived:true
+        });
+
+        recursiveArchive(args.id)
+        return document
     }
 })
 
