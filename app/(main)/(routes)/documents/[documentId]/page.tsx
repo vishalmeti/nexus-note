06fc96edcaf38810b27d0191b07fb3eb8a7b4b44
vishalmeti <vishalmeti9@gmail.com>
2024-06-14 20:55:42 +0530

    Added icon picker and document text area

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+"use client";
 
-const Page = () => {
-  return (
-    <div>Page</div>
-  )
+import Toolbar from "@/components/toolbar";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { useQuery } from "convex/react";
+
+interface DocumentIdPageProps {
+  params: {
+    documentId: Id<"documents">;
+  };
 }
 
-export default Page
+const Page = ({ params }: DocumentIdPageProps) => {
+  const document = useQuery(api.documents.getById, {
+    documentId: params.documentId,
+  });
+
+  if (document === undefined) {
+    return <div>Loading...</div>;
+  }
+  if (document === null) {
+    return <div className="">Not found</div>;
+  }
+  return <div className="pb-40">
+    <div className="h-[35vh]"/>
+    <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
+      <Toolbar initialData={document}/>
+    </div>
+  </div>;
+};
+
+export default Page;
