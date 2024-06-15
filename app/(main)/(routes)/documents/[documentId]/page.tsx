3f0eeb12c2c5ee70c41308f4c966b04ae033bb5a
vishalmeti <vishalmeti9@gmail.com>
2024-06-15 13:48:20 +0530

    rendering the uploaded cover image and removing the cover image.

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import { Cover } from "@/app/(main)/_components/cover";
 import Toolbar from "@/components/toolbar";
+import { Skeleton } from "@/components/ui/skeleton";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
-import { useQuery } from "convex/react";
+import { useMutation, useQuery } from "convex/react";
 
 interface DocumentIdPageProps {
   params: {
@@ -16,16 +18,38 @@ const Page = ({ params }: DocumentIdPageProps) => {
     documentId: params.documentId,
   });
 
+  const update = useMutation(api.documents.update)
+
+  const onChange = (content:string) => {
+    update({
+      id:params.documentId,
+      content
+    })
+  }
+
   if (document === undefined) {
-    return <div>Loading...</div>;
+    return (
+      <div>
+        <Cover.Skeleton/>
+        <div className="md:max-w-3xl lg:max-w-4xl mx-auto mt-10">
+          <div className="space-y-4 pl-8 pt-4">
+            <Skeleton className="h-14 w-[50%]"/>
+            <Skeleton className="h-14 w-[80%]"/>
+            <Skeleton className="h-14 w-[40%]"/>
+            <Skeleton className="h-14 w-[60%]"/>
+          </div>
+        </div>
+      </div>
+    )
   }
   if (document === null) {
     return <div className="">Not found</div>;
   }
   return <div className="pb-40">
-    <div className="h-[35vh]"/>
+    <Cover preview url={document?.coverImage} />
     <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
       <Toolbar initialData={document}/>
+      {/* <Editor editable={false} onChange={onChange} initialContent={document.content} /> */}
     </div>
   </div>;
 };
--- convex/documents.ts
+++ convex/documents.ts
@@ -345,31 +345,31 @@ export const getSearch = query({
     } 
   })
   
-//   export const removeCoverImage = mutation({
-//     args:{id:v.id('documents')},
-//     handler:async (context,args) => {
-//       const identity = await context.auth.getUserIdentity()
+  export const removeCoverImage = mutation({
+    args:{id:v.id('documents')},
+    handler:async (context,args) => {
+      const identity = await context.auth.getUserIdentity()
   
-//       if (!identity) {
-//         throw new Error("Unauthenticated")
-//       }
+      if (!identity) {
+        throw new Error("Unauthenticated")
+      }
   
-//       const userId = identity.subject
+      const userId = identity.subject
   
-//       const existingDocument = await context.db.get(args.id)
+      const existingDocument = await context.db.get(args.id)
   
-//       if (!existingDocument) {
-//         throw new Error('Not found')
-//       }
+      if (!existingDocument) {
+        throw new Error('Not found')
+      }
   
-//       if (existingDocument.userId !== userId) {
-//         throw new Error("Unauthorized")
-//       }
+      if (existingDocument.userId !== userId) {
+        throw new Error("Unauthorized")
+      }
   
-//       const document = await context.db.patch(args.id,{
-//         coverImage:undefined
-//       })
+      const document = await context.db.patch(args.id,{
+        coverImage:undefined
+      })
   
-//       return document
-//     }
-//   })
+      return document
+    }
+  })
